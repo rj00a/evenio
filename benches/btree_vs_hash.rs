bf9c092d80a73c9c6ece9de1a1843217f9f2f7e3
Ryan <ryanj00a@gmail.com>
2024-01-28 21:11:19 -0800

    Improve `btree_vs_hash`

--- benches/btree_vs_hash.rs
+++ benches/btree_vs_hash.rs
@@ -1,6 +1,7 @@
 //! Compares performance of hashsets and btreesets in relevant scenarios.
 
 use std::collections::BTreeSet;
+use std::hash::Hash;
 
 use ahash::AHashSet;
 use divan::{black_box, Bencher};
@@ -11,105 +12,85 @@ fn main() {
 
 const LENS: [u32; 6] = [1, 5, 10, 50, 100, 1000];
 
-#[divan::bench(args = LENS)]
-fn insert_u32_hashset(bencher: Bencher, len: u32) {
-    let mut set = AHashSet::<u32>::new();
+#[divan::bench(args = LENS, types = [u32, u64, u128])]
+fn insert_hashset<T>(bencher: Bencher, len: u32)
+where
+    T: From<u32> + Hash + Eq,
+{
+    let mut set = AHashSet::<T>::new();
     set.reserve(len as usize);
 
     bencher.bench_local(|| {
         for n in 0..len {
-            set.insert(black_box(n * 1000));
+            set.insert(black_box(T::from(n * 1000)));
         }
 
         set.clear();
     });
 }
 
-#[divan::bench(args = LENS)]
-fn insert_u32_btreeset(bencher: Bencher, len: u32) {
-    let mut set = BTreeSet::<u32>::new();
+#[divan::bench(args = LENS, types = [u32, u64, u128])]
+fn insert_btreeset<T>(bencher: Bencher, len: u32)
+where
+    T: From<u32> + Ord,
+{
+    let mut set = BTreeSet::<T>::new();
 
     bencher.bench_local(|| {
         for n in 0..len {
-            set.insert(black_box(n * 1000));
+            set.insert(black_box(T::from(n * 1000)));
         }
 
         set.clear();
     });
 }
 
-#[divan::bench(args = LENS)]
-fn lookup_u128_hashset(bencher: Bencher, len: u32) {
-    let set: AHashSet<u128> = (0..len).map(|n| u128::from(n) * 10000).collect();
+#[divan::bench(args = LENS, types = [u32, u64, u128])]
+fn lookup_hashset<T>(bencher: Bencher, len: u32)
+where
+    T: From<u32> + Hash + Eq + Sync,
+{
+    let set: AHashSet<T> = (0..len).map(|n| T::from(n * 1000)).collect();
 
     bencher.bench(|| {
-        black_box(set.get(&black_box(123456789)));
+        black_box(set.get(&black_box(T::from(123456789))));
     });
 }
 
-#[divan::bench(args = LENS)]
-fn lookup_u128_btreeset(bencher: Bencher, len: u32) {
-    let set: BTreeSet<u128> = (0..len).map(|n| u128::from(n) * 10000).collect();
+#[divan::bench(args = LENS, types = [u32, u64, u128])]
+fn lookup_btreeset<T>(bencher: Bencher, len: u32)
+where
+    T: From<u32> + Ord + Sync,
+{
+    let set: BTreeSet<T> = (0..len).map(|n| T::from(n * 1000)).collect();
 
     bencher.bench(|| {
-        black_box(set.get(&black_box(123456789)));
+        black_box(set.get(&black_box(T::from(123456789))));
     });
 }
 
-#[divan::bench(args = LENS)]
-fn lookup_u128_linear_search(bencher: Bencher, len: u32) {
-    let set: Vec<u128> = (0..len).map(|n| u128::from(n) * 10000).collect();
+#[divan::bench(args = LENS, types = [u32, u64, u128])]
+fn lookup_linear_search<T>(bencher: Bencher, len: u32)
+where
+    T: From<u32> + Eq + Sync,
+{
+    let set: Vec<T> = (0..len).map(|n| T::from(n * 1000)).collect();
 
     bencher.bench(|| {
-        let n = black_box(123456789);
-        black_box(set.iter().find(|&&v| v == n));
+        let n = black_box(T::from(123456789));
+        black_box(set.iter().find(|&v| *v == n));
     });
 }
 
-#[divan::bench(args = LENS)]
-fn lookup_128_binary_search(bencher: Bencher, len: u32) {
-    let set: Vec<u128> = (0..len).map(|n| u128::from(n) * 10000).collect();
+#[divan::bench(args = LENS, types = [u32, u64, u128])]
+fn lookup_binary_search<T>(bencher: Bencher, len: u32)
+where
+    T: From<u32> + Ord + Sync,
+{
+    let set: Vec<T> = (0..len).map(|n| T::from(n * 1000)).collect();
 
     bencher.bench(|| {
-        let n = black_box(123456789);
-        let _ = black_box(set.binary_search(&n));
-    });
-}
-
-#[divan::bench(args = LENS)]
-fn lookup_u32_hashset(bencher: Bencher, len: u32) {
-    let set: AHashSet<u32> = (0..len).map(|n| n * 10000).collect();
-
-    bencher.bench(|| {
-        black_box(set.get(&black_box(123456789)));
-    });
-}
-
-#[divan::bench(args = LENS)]
-fn lookup_u32_btreeset(bencher: Bencher, len: u32) {
-    let set: BTreeSet<u32> = (0..len).map(|n| n * 10000).collect();
-
-    bencher.bench(|| {
-        black_box(set.get(&black_box(123456789)));
-    });
-}
-
-#[divan::bench(args = LENS)]
-fn lookup_u32_linear_search(bencher: Bencher, len: u32) {
-    let set: Vec<u32> = (0..len).map(|n| n * 10000).collect();
-
-    bencher.bench(|| {
-        let n = black_box(123456789);
-        black_box(set.iter().find(|&&v| v == n));
-    });
-}
-
-#[divan::bench(args = LENS)]
-fn lookup_u32_binary_search(bencher: Bencher, len: u32) {
-    let set: Vec<u32> = (0..len).map(|n| n * 10000).collect();
-
-    bencher.bench(|| {
-        let n = black_box(123456789);
+        let n = black_box(T::from(123456789));
         let _ = black_box(set.binary_search(&n));
     });
 }
