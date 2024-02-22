23f55c1e2969ce274768009cffad0acf32356cbe
Ryan <ryanj00a@gmail.com>
2024-02-22 01:56:01 -0800

    Add `IndexSet` to `btree_vs_hash` benchmark

--- benches/btree_vs_hash.rs
+++ benches/btree_vs_hash.rs
@@ -6,6 +6,7 @@ use std::hash::Hash;
 
 use ahash::AHashSet;
 use divan::{black_box, Bencher};
+use indexmap::IndexSet;
 
 fn main() {
     divan::main()
@@ -19,6 +20,23 @@ where
     T: From<u32> + Hash + Eq,
 {
     let mut set = AHashSet::<T>::new();
+    set.reserve(len as usize);
+
+    bencher.bench_local(|| {
+        for n in 0..len {
+            set.insert(black_box(T::from(n * 1000)));
+        }
+
+        set.clear();
+    });
+}
+
+#[divan::bench(args = LENS, types = [u32, u64, u128])]
+fn insert_indexset<T>(bencher: Bencher, len: u32)
+where
+    T: From<u32> + Hash + Eq,
+{
+    let mut set = IndexSet::with_hasher(ahash::RandomState::new());
     set.reserve(len as usize);
 
     bencher.bench_local(|| {
@@ -52,6 +70,18 @@ where
     T: From<u32> + Hash + Eq + Sync,
 {
     let set: AHashSet<T> = (0..len).map(|n| T::from(n * 1000)).collect();
+
+    bencher.bench(|| {
+        black_box(set.get(&black_box(T::from(123456789))));
+    });
+}
+
+#[divan::bench(args = LENS, types = [u32, u64, u128])]
+fn lookup_indexset<T>(bencher: Bencher, len: u32)
+where
+    T: From<u32> + Hash + Eq + Sync,
+{
+    let set: IndexSet<T, ahash::RandomState> = (0..len).map(|n| T::from(n * 1000)).collect();
 
     bencher.bench(|| {
         black_box(set.get(&black_box(T::from(123456789))));
@@ -102,6 +132,20 @@ const ARRAY_COUNT: usize = 100;
 #[divan::bench(args = ARRAY_LENS)]
 fn lookup_u32_array_hashset(bencher: Bencher, array_len: usize) {
     let set: AHashSet<Box<[u32]>> = (0..ARRAY_COUNT)
+        .map(|n| vec![n as u32; n % array_len + 1].into_boxed_slice())
+        .chain([[].into()])
+        .collect();
+
+    let val = vec![12345; array_len].into_boxed_slice();
+
+    bencher.bench(|| {
+        black_box(set.get(black_box(&*val)));
+    });
+}
+
+#[divan::bench(args = ARRAY_LENS)]
+fn lookup_u32_array_indexset(bencher: Bencher, array_len: usize) {
+    let set: IndexSet<Box<[u32]>, ahash::RandomState> = (0..ARRAY_COUNT)
         .map(|n| vec![n as u32; n % array_len + 1].into_boxed_slice())
         .chain([[].into()])
         .collect();
